45740c21ec3a3b0d73be276b08e869ce06177cdf
Mortimoos <krimones@.com>
2025-05-13 04:17:49 +0200

    did an oopsie, fixed it tho

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,5 +1,5 @@
 import {Outlet} from 'react-router-dom'
-import Navbar from "../pages/dashboard/Navbar.tsx";
+import Navbar from "../components/dashboard/Navbar.tsx";
 
 function DashboardLayout() {
     return (
