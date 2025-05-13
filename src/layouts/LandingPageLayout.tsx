e66bd19dd20e14201a2f9e71cfb3b78fab890503
Mortimoos <krimones@.com>
2025-05-13 04:57:47 +0200

    navbar example with tailwind.

--- src/layouts/LandingPageLayout.tsx
+++ src/layouts/LandingPageLayout.tsx
@@ -3,7 +3,6 @@ import {Outlet} from "react-router-dom";
 function LandingPageLayout() {
     return (
             <div className="LandingPageParent">
-                <h1>This is a Landing Page placeholder.</h1>
                 <Outlet/>
             </div>
     )
--- src/pages/landingPage/LandingPage.tsx
+++ src/pages/landingPage/LandingPage.tsx
@@ -4,7 +4,10 @@ import Navbar from "../../components/landingPage/Navbar.tsx";
 const LandingPage: React.FC = () => {
     return (
         <>
-            <Navbar />
+            {/*gotta override root styling here*/}
+            <div className="w-full fixed top-0 left-0 z-50">
+                <Navbar />
+            </div>
         </>
     );
 };
