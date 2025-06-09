155fbe2989f871dccddd1217f1364dc961bc48d0
Marko Intihar <intihar.marko123@gmail.com>
2025-06-09 11:55:53 +0200

    skusanje deployat 2

--- src/components/dashboard/TutorialModal.tsx
+++ src/components/dashboard/TutorialModal.tsx
@@ -1,54 +1,44 @@
 // TutorialModal.tsx
 import { useEffect, useState } from "react";
-import { Description, Dialog } from "@headlessui/react";
+import { Dialog } from "@headlessui/react";
 import { motion, AnimatePresence } from "framer-motion";
 import { ChevronRight, ChevronLeft, X } from "lucide-react";
 
 const steps = [
   {
-    image: "/tutorial/step1.png",
+    image: "/tutorial/tutorial1.png",
     title: "Space Creation",
     description: "To create a \"space\" we give it a name and press +Add button. To view the space we press View button under the space we just created"
   },
   {
-    image: "/tutorial/setGrid1.png",
+    image: "/tutorial/tutorial2.png",
     title: "Setting up the grid",
     description: "To change the size of the grid we press the \"set grid\" button."
   },
   {
-    image: "/tutorial/setGrid2.png",
+    image: "/tutorial/tutorial3.png",
     title: "Setting up the grid",
-    description: "We are prompted with a imput form where we are able to add any number of rows or columns on any side of the grid. When we are happy with changes we press apply."
+    description: "We are prompted with a imput form where we are able to add any number of rows or columns on any side of the grid and a map to select location of our space. When we are happy with changes we press apply."
   },
   {
-    image: "/tutorial/editMode2.png",
+    image: "/tutorial/tutorial14.png",
     title: "Editing the space",
     description: "We provide the ability to mark each grid cell as either inside or outside a defined area. This is done by clicking the \"Inside / Outside\" button and using the mouse to highlight the appropriate cells. If a mistake is made, it can easily be corrected using the \"Eraser\" tool."
   },
   {
-    image: "/tutorial/editMode3.png",
+    image: "/tutorial/tutorial5.png",
     title: "Editing the space",
     description: "To add plants to the grid, click the \"Add Plant\" button."
   },
   {
-    image: "/tutorial/editMode4.png",
+    image: "/tutorial/tutorial6.png",
     title: "Editing the space",
-    description: "A searchable list of available plants will appear. Simply find the plant you wish to add and click to select it."
+    description: "To enable watering reminders for your plants, simply click the corresponding button. The reminder system will notify you when it's time to water and will intelligently skip rainy days."
   },
   {
-    image: "/tutorial/editMode5.png",
+    image: "/tutorial/tutorial7.png",
     title: "Editing the space",
-    description: "Use the mouse to highlight the cells where the selected plant should be placed. Once you're satisfied with the placement, click the \"Stop Planting\" button."
-  },
-    {
-    image: "/tutorial/editMode6.png",
-    title: "Saving the space",
     description: "Once you're satisfied with your grid layout, click the \"Save Table\" button to save your changes."
-  },
-    {
-    image: "/tutorial/editMode5.png",
-    title: "Turning on reminders",
-    description: "To enable watering reminders for your plants, simply click the corresponding button. The reminder system will notify you when it's time to water and will intelligently skip rainy days."
   },
 ];
 
