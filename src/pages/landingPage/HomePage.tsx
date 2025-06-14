46dc9f78d3b86edbc30b6270450dbcafdbcf0f6d
Mortimoos <krimones@.com>
2025-06-14 15:29:19 +0200

    quickfix

--- src/pages/landingPage/HomePage.tsx
+++ src/pages/landingPage/HomePage.tsx
@@ -11,7 +11,7 @@ const HomePage: React.FC = () => {
         <>
             <HeroSection />
             <ToolsSection />
-            <AssistantSection />
+            {/*<AssistantSection />*/}
             <CTASection />
             <TestimonialsSection />
             <FAQSection />
